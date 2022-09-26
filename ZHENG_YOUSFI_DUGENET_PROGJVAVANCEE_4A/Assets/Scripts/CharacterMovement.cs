dc540c7ecc6af9e5e9c5565a960c9c2cd80baa4b
KrymoXCIII <karym.yousfi@gmail.com>
2022-09-26 16:17:02 +0200

    a

--- ZHENG_YOUSFI_DUGENET_PROGJVAVANCEE_4A/Assets/Scripts/CharacterMovement.cs
+++ ZHENG_YOUSFI_DUGENET_PROGJVAVANCEE_4A/Assets/Scripts/CharacterMovement.cs
@@ -1,19 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
     public int speed;
     public GameObject bomb;
-    private bool _bombReady;
-    private float _countDown;
-    
-    
+
+     Player Player;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        _bombReady = true;
+        Player = FindObjectOfType<Player>();
     }
 
     // Update is called once per frame
@@ -21,43 +23,28 @@ public class CharacterMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Z))
         {
-            gameObject.transform.Translate(new Vector3(-speed,0,0) * Time.deltaTime);
+            gameObject.transform.Translate(new Vector3(-speed, 0, 0) * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            gameObject.transform.Translate(new Vector3(speed,0,0) * Time.deltaTime);
+            gameObject.transform.Translate(new Vector3(speed, 0, 0) * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.Q))
         {
-            gameObject.transform.Translate(new Vector3(0,0,-speed) * Time.deltaTime);
+            gameObject.transform.Translate(new Vector3(0, 0, -speed) * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            gameObject.transform.Translate(new Vector3(-0,0,speed) * Time.deltaTime);
+            gameObject.transform.Translate(new Vector3(-0, 0, speed) * Time.deltaTime);
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (_bombReady)
-            {
-                _countDown = Time.time;
-                bomb.transform.position = new Vector3(gameObject.transform.position.x,gameObject.transform.position.y ,gameObject.transform.position.z );
-                Instantiate(bomb);
-                _bombReady = false;
-                
-            }
-           
+            Player.PlantBomb(bomb, gameObject);
+
         }
-        // Compte à rebours
-        if (Time.time - _countDown >= 5)
-        {
-            Destroy(bomb);
-            _bombReady = true;
-            
-        }
-        
     }
 }
