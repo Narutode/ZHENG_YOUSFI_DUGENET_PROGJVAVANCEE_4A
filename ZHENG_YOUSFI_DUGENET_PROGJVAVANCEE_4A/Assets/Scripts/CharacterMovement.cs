26bb78e01628a04293152a5abebee4c0ab95c170
KrymoXCIII <karym.yousfi@gmail.com>
2022-09-26 14:23:25 +0200

    test

---
+++ ZHENG_YOUSFI_DUGENET_PROGJVAVANCEE_4A/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            gameObject.transform.Translate(new Vector3(-speed,0,0) * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.transform.Translate(new Vector3(speed,0,0) * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            gameObject.transform.Translate(new Vector3(0,0,speed) * Time.deltaTime);
+        }
+        
+    }
+}
